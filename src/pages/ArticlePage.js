4265fb6e6b3c85c5bc07f8024fa00b2854a69633
pragatibhandari <bhandariprag7@gmail.com>
2023-01-15 14:16:45 +0200

    react router added

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,5 @@
+const ArticlePage = () => {
+  return <h1>This is Article page!</h1>;
+};
+
+export default ArticlePage;
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,5 @@
+const ArticlesListPage = () => {
+  return <h1>This is ArticlesList page!</h1>;
+};
+
+export default ArticlesListPage;
